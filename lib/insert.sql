1d3f9cdde64a1f3a33af43d35ca28bf2dd9e05bc
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-05-21 19:46:34 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 7, "Male", "Brown", "Happy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, "Male", "Brown", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 13, "Female", "Black", "Complacent", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, "Male", "Brown", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 4, "Female", "Brown", "Sad", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 12, "Male", "Black", "Angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 7, "Female", "Brown", "Happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, "Brown", "Angry", 1);
